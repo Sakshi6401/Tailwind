6b1364e588e5f2fc9db481b48db2126e3123759c
Sakshi Gairola <1906499@kiit.ac.in>
2021-02-21 15:51:04 +0530

    learning about bg-color-shade and {w/h} classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-200">
+      <div className='bg-blue-100 w-5 h-6'>Box1</div> 
+      <div className='bg-blue-200 w-5 h-6'>Box2</div> 
+      <div className='bg-blue-300 w-5 h-6'>Box3</div> 
+      <div className='bg-blue-400 w-5 h-6'>Box4</div> 
+      <div className='bg-blue-500 w-5 h-6'>Box5</div>
+      <div className='bg-blue-600 w-5 h-6'>Box6</div> 
+      <div className='bg-blue-700 w-5 h-6'>Box7</div> 
+      <div className='bg-blue-800 w-5 h-6'>Box8</div> 
+      <div className='bg-blue-900 w-5 h-6'>Box9</div>  
     </div>
   );
 }
