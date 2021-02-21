1da3a0a9906fefd795b678beea3ec4c9484aeefc
Sakshi Gairola <1906499@kiit.ac.in>
2021-02-21 16:09:21 +0530

    learned margin and padding

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-gray-200">
-      <div className='bg-blue-100 w-5 h-6'>Box1</div> 
-      <div className='bg-blue-200 w-5 h-6'>Box2</div> 
-      <div className='bg-blue-300 w-5 h-6'>Box3</div> 
-      <div className='bg-blue-400 w-5 h-6'>Box4</div> 
-      <div className='bg-blue-500 w-5 h-6'>Box5</div>
-      <div className='bg-blue-600 w-5 h-6'>Box6</div> 
-      <div className='bg-blue-700 w-5 h-6'>Box7</div> 
-      <div className='bg-blue-800 w-5 h-6'>Box8</div> 
-      <div className='bg-blue-900 w-5 h-6'>Box9</div>  
+    <div className='h-full'>
+      <div className='bg-blue-100 w-32 h-32 mb-4 px-5'>Box1</div> 
+      <div className='bg-blue-200 w-32 h-6 mt-4 py-5'>Box2</div> 
+      <div className='bg-blue-300 w-32 h-32 ml-4'>Box3</div> 
+      <div className='bg-blue-400 w-32 h-6 mb-4'>Box4</div> 
+      <div className='bg-blue-500 w-32 h-32 mb-4 p-10'>Box5</div>
+      <div className='bg-blue-600 w-32 h-6 mt-4'>Box6</div> 
+      <div className='bg-blue-700 w-32 h-32 ml-4'>Box7</div> 
+      <div className='bg-blue-800 w-32 h-6 mr-4'>Box8</div> 
+      <div className='bg-blue-900 w-32 h-32 m-4'>Box9</div>  
     </div>
+
   );
 }
 
