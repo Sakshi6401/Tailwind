729ad28205712fd55737e68ae892e20140c2161f
Sakshi Gairola <1906499@kiit.ac.in>
2021-02-21 18:37:40 +0530

    practiced flexbox

--- src/practice/flex.js
+++ src/practice/flex.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Flex = () => (
-    <div className='flex'>
+    <div class="h-screen flex flex-wrap-reverse">
+        <div class="bg-yellow-600 w-16 h-16">1</div>
+        <div class="bg-blue-700 w-16 h-16">2</div>
+        <div class="bg-red-700 w-16 h-16">3</div>
+        <div class="bg-yellow-600 w-16 h-16">1</div>
+        <div class="bg-blue-700 w-16 h-16">2</div>
+        <div class="bg-red-700 w-16 h-16">3</div>
+        <div class="bg-yellow-600 w-16 h-16">1</div>
+        <div class="bg-blue-700 w-16 h-16">2</div>
+        <div class="bg-red-700 w-16 h-16">3</div>
+        <div class="bg-yellow-600 w-16 h-16">1</div>
+        <div class="bg-blue-700 w-16 h-16">2</div>
+        <div class="bg-red-700 w-16 h-16">3</div>
         <div class="bg-yellow-600 w-16 h-16">1</div>
         <div class="bg-blue-700 w-16 h-16">2</div>
         <div class="bg-red-700 w-16 h-16">3</div>
