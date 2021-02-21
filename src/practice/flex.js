95f670edd060b739e829c926297036930233fe42
Sakshi Gairola <1906499@kiit.ac.in>
2021-02-21 19:03:57 +0530

    completed flex box challenge

--- src/practice/flex.js
+++ src/practice/flex.js
@@ -1,23 +1,35 @@
 import React from 'react';
 
 const Flex = () => (
-    <div class="h-screen flex flex-wrap-reverse">
-        <div class="bg-yellow-600 w-16 h-16">1</div>
-        <div class="bg-blue-700 w-16 h-16">2</div>
-        <div class="bg-red-700 w-16 h-16">3</div>
-        <div class="bg-yellow-600 w-16 h-16">1</div>
-        <div class="bg-blue-700 w-16 h-16">2</div>
-        <div class="bg-red-700 w-16 h-16">3</div>
-        <div class="bg-yellow-600 w-16 h-16">1</div>
-        <div class="bg-blue-700 w-16 h-16">2</div>
-        <div class="bg-red-700 w-16 h-16">3</div>
-        <div class="bg-yellow-600 w-16 h-16">1</div>
-        <div class="bg-blue-700 w-16 h-16">2</div>
-        <div class="bg-red-700 w-16 h-16">3</div>
-        <div class="bg-yellow-600 w-16 h-16">1</div>
-        <div class="bg-blue-700 w-16 h-16">2</div>
-        <div class="bg-red-700 w-16 h-16">3</div>
-    </div>
+    <div class="h-screen flex flex-wrap flext-between">
+            <div className='flex h-1/3 w-1/3'>
+                <div class="bg-blue-100 m-2 w-full flex items-center justify-center">A</div>
+            </div>
+            <div className='flex h-1/3 w-1/3'>
+                <div class="bg-blue-200 m-2 w-full flex items-center justify-center">B</div>
+            </div>
+            <div className='flex h-1/3 w-1/3'>
+                <div class="bg-blue-300 m-2 w-full flex items-center justify-center">C</div>
+            </div>
+            <div className='flex h-1/3 w-1/3'>
+                <div class="bg-blue-400 m-2 w-full flex items-center justify-center">D</div>
+            </div>
+            <div className='flex h-1/3 w-1/3'>
+                <div class="bg-blue-500 m-2 w-full flex items-center justify-center">E</div>
+            </div>
+            <div className='flex h-1/3 w-1/3'>
+                <div class="bg-blue-600 m-2 w-full flex items-center justify-center">F</div>
+            </div>
+            <div className='flex h-1/3 w-1/3'>
+                <div class="bg-blue-700 m-2 w-full flex items-center justify-center">G</div>
+            </div>
+            <div className='flex h-1/3 w-1/3'>
+                <div class="bg-blue-800 m-2 w-full flex items-center justify-center">H</div>
+            </div>
+            <div className='flex h-1/3 w-1/3'>
+                <div class="bg-blue-900 m-2 w-full flex items-center justify-center">I</div>            
+            </div>
+        </div>
 );
 
 export default Flex;
