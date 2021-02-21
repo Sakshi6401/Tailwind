145ac11ed13e5d49e5b15185b92984e39ea3bf53
Sakshi Gairola <1906499@kiit.ac.in>
2021-02-21 17:22:05 +0530

    completed text formatting part including the small challenge given

--- src/practice/text.js
+++ src/practice/text.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 const Text = () => (
     <div className='m-3 font-sans'>
-        <p className='p-5 text-sm text-left bg-blue-900 text-blue-500'>
+        <h1 className='capitalize text-xl text-gray-900'>Lorem ipsum dolor sit amet consectetur.</h1>
+
+        <p className='mt-3 tracking-wide leading-loose'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit orci ac nisl varius varius. Nullam auctor finibus pulvinar. Morbi porttitor placerat enim nec consequat.
         </p>
-        <p className='p-5 text-base text-right bg-red-900 text-red-400'>
+        
+        <div className='m-2 p-4 bg-gray-900 text-gray-400'>
+            <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit orci ac nisl varius varius. Nullam auctor finibus pulvinar. Morbi porttitor placerat enim nec consequat.
-        </p>
-        <p className='p-5 text-2xl text-center bg-gray-900 text-gray-500'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit orci ac nisl varius varius. Nullam auctor finibus pulvinar. Morbi porttitor placerat enim nec consequat.
-        </p>
-        <p className='p-5 text-lg text-justify bg-green-900 text-green-400'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit orci ac nisl varius varius. Nullam auctor finibus pulvinar. Morbi porttitor placerat enim nec consequat.
-        </p>
+            </p>
+            <p className='mt-3 text-gray-300'>- Victor Gonzalez, <span className='italic'>instructor</span></p>
+        </div>
+        
+        <button className='bg-blue-500 text-blue-200 uppercase px-4 py-3'>Enroll Now</button>
     </div>
 );
 
